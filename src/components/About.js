fc354ec23074454d768df0b981b13936e68965a2
Harsh Sinha <85688366+harsh1sinha@users.noreply.github.com>
2024-01-20 23:34:53 +0530

    about us

---
+++ src/components/About.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import img1 from '../image/img1.png'
+import img2 from '../image/img2.png'
+import img8 from '../image/img8.png'
+import img7 from '../image/img7.png'
+import img9 from '../image/img9.jpeg'
+
+
+const About = () => {
+  return (
+    <div id="about" class="fea-area padding-top-120 padding-bottom-90">
+        <div class="fea-shapes">
+            
+            <img src="assets/img/shape/e_vec3.png" alt="" class="vec_1"/>
+            <img src="assets/img/shape/e_vec5.png" alt="" class="vec_2"/>
+        </div>
+        <div class="container position-relative">
+            <div class="row align-items-center">
+                <div class="col-lg-6 wow slideInLeft">
+                    <div class="fea-left">
+                        <div class="common-title">
+                            <h5>About Us</h5>
+                            <h2 style={{color:'#40e0d0'}}>learn new <span>things and connect</span> with awesome people</h2>
+                            <p class="margin-top-20">
+                            The Cynaptics Club stands as IIT Indore's premier student body exclusively focused on Machine Learning. Demonstrating a proactive engagement with the cutting-edge trends in Machine Learning and AI, the club is dedicated to leveraging these technologies for the betterment of society. Through impactful projects with social relevance and the organization of workshops and talk sessions, the Cynaptics Club is a dynamic hub connecting students with distinguished individuals leading advancements in AI/ML across diverse domains.
+
+                            <p>
+                            <br />
+                            Google Developer Student Club IITI (GDSC-IITI), a collaborative venture with Google Developers, stands as a vibrant club catering to passionate students within global developer communities.This unique platform is dedicated to bridging the gap between theoretical knowledge and practical application. Focusing on Android, Web, Cloud, and Data Science, GDSC-IITI offers students the opportunity not only to learn through peer-to-peer workshops but also to actively contribute to solving real-world problems for the local community through sponsored projects.
+                            </p>
+                            </p>
+
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="fea-right">
+                        <div class="row">
+                            <div class="col-12 wow fadeInUp">
+                                <img src={img7} alt="" style={{border:'5px solid #7F89FF', boxShadow:'0 0 10px 2px #48abe0'}}/>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6 wow fadeInDown">
+                                <img src={img8} alt="" style={{border:'5px solid #7F89FF', boxShadow:'0 0 10px 2px #48abe0'}}/>
+                            </div>
+                            <div class="col-6 wow fadeInDown" data-wow-delay=".2s">
+                                <img src={img9} alt="" style={{border:'5px solid #7F89FF', boxShadow:'0 0 10px 2px #48abe0'}}/>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 wow fadeInUp">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/EventPg.js
@@ -0,0 +1,81 @@
+import React from 'react'
+
+const EventPg = () => {
+    return (
+        <div class="event-promo padding-bottom-90">
+            
+            <div class="container position-relative">
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <div class="ep_left">
+                            <div class="row">
+                                <div class="col-md-6 col-sm-12 d-md-flex align-items-center wow fadeInUp"
+                                    data-wow-delay=".4s">
+                                    <div class="single-ep">
+                                        <div class="icon-box">
+                                            <i class="far fa-microphone-alt"></i>
+                                        </div>
+                                        <div class="cont-box">
+                                            <h4 style={{color:'#FC414D'}}>Learn from others</h4>
+                                            <p class="margin-top-20">Not only do you get to learn while making your own
+                                                project, but you can also check out other projects once the hackathon is
+                                                over to see what others did differently.
+                                            </p>
+    
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 wow fadeInUp" data-wow-delay=".2s">
+                                    <div class="single-ep">
+                                        <div class="icon-box">
+                                            <i class="far fa-share-alt"></i>
+                                        </div>
+                                        <div class="cont-box">
+                                            <h4 style={{color:'#FC414D'}}>Build Your Network</h4>
+                                            <p class="margin-top-20 ">You'll get exposure to some of the best minds in the
+                                                country, not just among the participants, but also the judges. You can make
+                                                friends and forge professional relationships with people from all across the
+                                                country.
+                                            </p>
+    
+                                        </div>
+                                    </div>
+                                    <div class="single-ep">
+                                        <div class="icon-box">
+                                            <i class="far fa-users"></i>
+                                        </div>
+                                        <div class="cont-box">
+                                            <h4 style={{color:'#FC414D'}}>Satisfy the coder within</h4>
+                                            <p class="margin-top-20">That feeling of pleasure you get when you fix a bug
+                                                after searching for hours and your program finally works? Almost every coder
+                                                knows that feeling. Come hunt for it here.
+                                            </p>
+    
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 wow fadeInUp" data-wow-delay=".6s">
+                        <div class="ep_right">
+                            <div class="common-title">
+                                <h5 >Why Join ÉNOSIUM</h5>
+                                <h2 style={{color:'#40e0d0'}}>Why You Should <span>Join Event</span> ?</h2>
+                                <p class="margin-top-20">
+                                Énosium, a hackathon organized by the Google Developers Students Club chapter and the Cynaptics Club at IIT Indore, converges the in-demand skill of Machine Learning (ML) with the well-established proficiency in Web Development. ML's pervasive applications and the foundational role of Web Development form the bedrock of contemporary smart systems. This event aims to explore the synergies between these two domains, recognizing their individual strengths and fostering collaboration to unlock new possibilities at the intersection of ML and Web Development.
+                                </p>
+                                <p class="margin-top-20">
+                                The website serves as the first point of contact for users engaging with a digital product. The efficacy of your website design significantly influences the potential success of your product. Acknowledging the paramount importance of both website design and product success, we've created a platform for enthusiasts to delve into and exhibit the harmonious convergence of these two crucial domains.
+                                </p>
+                                <a href="https://unstop.com/p/enosium-fluxus-indian-institute-of-technology-iit-indore-576408" class="btn1 margin-top-10 ">join event</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      );
+}
+
+export default EventPg
