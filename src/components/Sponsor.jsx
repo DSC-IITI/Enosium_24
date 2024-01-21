977085aedcedcdce2d9de9f56e8dd38587a2ce30
unknown <ishitapandey200423@gmail.com>
2024-01-21 16:49:26 +0530

    sponsor updated

--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -3,6 +3,7 @@ import './Sponsor.css';
 
 export default  function Sponsor() {
     return (
+        <>
         <div id="sponsor" class="brand-area padding-top-110 padding-bottom-90">
         <div class="container">
             <div class="row justify-content-center margin-bottom-20">
@@ -77,6 +78,50 @@ export default  function Sponsor() {
             </div>
         </div>
     </div>
+    <div id="speaker" class="speaker-area padding-top-110 padding-bottom-120">
+    <div class="spe-shapes">
+        <img src="assets/img/shape/e_vec5.png" alt="" class="vec_1" />
+        <img src="assets/img/shape/vec_1.png" alt="" class="vec_2 item-rotate" />
+        <img src="assets/img/shape/e_vec9.png" alt="" class="vec_3 d-none d-md-block" />
+    </div>
+    <div class="container">
+        <div class="row justify-content-center margin-bottom-20">
+            <div class="col-lg-6">
+                <div class="common-title text-center">
+                    <h5>Community Partners</h5>
+                    <h2 style={{color:'turquoise'}}>meet our <span>Community Partners</span></h2>
+                    <p class="margin-top-20">
+                        We are elated to present to you our supportive team of our amazing community partners, who stand by us to further transcend our team.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-center is">
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12 wow zoomIn" data-wow-delay=".4s">
+                <div class="single-brand isdivide">
+                    <a href="https://fluxus.co.in/"><img className='isimage' src="assets/img/speaker/fluxus.jpeg" alt="" /></a>
+                </div>
+            </div> 
+                     
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12 wow zoomIn" data-wow-delay=".4s">
+                <div  class="single-brand isdivide">
+                   <a href="https://unstop.com/hackathon/enosium-indian-institute-of-technology-iit-indore-576408"> <img className='isimage' src="assets/img/speaker/ingenium.jpeg" alt="" /></a>
+                </div>
+            </div>
+            
+            
+        </div>
+        <div class="row justify-content-center" >
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12 wow zoomIn" data-wow-delay=".4s">
+                <div class="single-brand isdivide">
+                    <a href="https://gdsc.community.dev/school-of-data-science-and-forecasting-indore/"><img className='isimage' src="assets/img/speaker/Screenshot 2023-01-09 at 10.37.08 AM.png" alt="" /></a>
+                </div>
+            </div> 
+        </div>
+    </div>
+</div>
+    </>
 
 
 
